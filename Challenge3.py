b0bae2ef139ef192c74ee069ec831cde3cf4d9e1
Shamil <shamil.sqa@gmail.com>
2020-05-04 00:46:49 -0600

    updates for Challenge3

--- Challenge3.py
+++ Challenge3.py
@@ -1,3 +1,5 @@
+import py as py
+from selenium.webdriver.common.keys import Keys
 
 
 
@@ -5,6 +7,23 @@ def test_challenge3(copart):
     for car in copart.find("[ng-repeat*='popularSearch'] > a"):
         href = car.get_attribute('href')
         print(f'{car.text} - {href}')
+
+
+def test_challenge4_copart(py):
+    py.visit('https://copart.com')
+    popular_models = py.find("li[ng-repeat*='popularSearch'] > a")
+    for model in popular_models:
+        print(f'{model.text} - {model.get_attribute("href")}')
+    assert popular_models.length == 20
+
+
+
+
+
+
+
+
+
 
 
 
