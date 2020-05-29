4d6180d05f14b45abc38f0a0b5cdab415838a3bb
Shamil <shamil.sqa@gmail.com>
2020-05-29 00:01:20 -0600

    Popular_Make/&Models_List

--- Challenge3.py
+++ Challenge3.py
@@ -1,6 +1,4 @@
-import py as py
-from selenium.webdriver.common.keys import Keys
-
+from time import sleep, time
 
 
 def test_challenge3(copart):
@@ -9,14 +7,25 @@ def test_challenge3(copart):
         print(f'{car.text} - {href}')
 
 
-def test_challenge4_copart(py):
+def test_challenge3_copart(py):
     py.visit('https://copart.com')
     popular_models = py.find("li[ng-repeat*='popularSearch'] > a")
     for model in popular_models:
-        print(f'{model.text} - {model.get_attribute("href")}')
-    assert popular_models.length == 20
+        print(f'{model.text()} - {model.get_attribute("href")}')
+    assert py.should('popular_models.length == 20')
 
 
+def test_challenge3_copart_honda_search(py):
+    py.visit('https://copart.com')
+    py.get('[data-uname="homePageFindAVehicle"]').click()
+    py.get('[data-uname="vehicleFinderTab"]').click()
+    py.get("[data-uname=\"vehiclefinderTypedropdownbox\"]").select('Automobile')
+    py.get('[data-uname="vehiclefinderMakedownbox"]').select('Honda')
+    py.get('[data-uname="vehiclefinderModeldownbox"]').select('CRV')
+    py.get('[data-uname="vehiclefinderLocationdropdownbox"]').select('UT - Salt Lake City')
+    py.get('[ng-click="search(form)"]').click()
+    assert py.should().contain_title('Honda')
+    py.get('[name="serverSideDataTable_length"]').select('100').click()
 
 
 
