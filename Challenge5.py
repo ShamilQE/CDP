72aa91aabca7afeb3563820a53115cef19f89ace
Shamil <shamil.sqa@gmail.com>
2020-05-29 00:02:41 -0600

    Models_Porsche

--- Challenge5.py
+++ Challenge5.py
@@ -25,7 +25,6 @@ def test_copart_porsche_model_count(py):
     py.get('#input-search').type('Porsche')
     py.get('[type="submit"]').click()
     py.get('[name="serverSideDataTable_length"]').select('100').click()
-    #py.wait(5)
     assert py.find('[data-uname="lotsearchLotnumber"]').should().have_length(100)
 
     lotsearchLotmodel = dict()
